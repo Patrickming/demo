c8fb79438ec4378d0818d60b854e2596ab359728
Patrick <patrickmingx@gmail.com>
2022-11-15 15:23:22 +0800

    demo commit2

--- src/main/java/com/demo/test.java
+++ src/main/java/com/demo/test.java
@@ -10,5 +10,7 @@ public class test {
         System.out.println("hello git ");
         System.out.println("hello git2 ");
         System.out.println("hello git3 ");
+        System.out.println("demo git ");
+
     }
 }
