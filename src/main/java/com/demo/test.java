abe89f732bea0ce3c7bf43621a7810637c96b957
Patrick <patrickmingx@gmail.com>
2022-11-15 15:10:36 +0800

    first commit

---
+++ src/main/java/com/demo/test.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+/**
+ * @author: xrm
+ * @date: 2022/11/15 15:03
+ * @description:
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello git ");
+    }
+}
